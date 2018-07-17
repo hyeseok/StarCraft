62ea35390a153258489b85c0595544c1347d6528
hyeseok <41276412+hyeseok@users.noreply.github.com>
2018-07-17 14:27:47 +0900

    절차지향적 코드

---
+++ StarCraft/StarCraft/Marine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarCraft
+{
+    class Marine
+    {
+        public int HP = 50;
+        public int speed = 1;
+    }
+}
---
+++ StarCraft/StarCraft/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarCraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Marine m1 = new Marine();
+
+            //m1 스팀팩 사용
+            m1.HP = m1.HP - 5;
+            m1.speed = m1.speed + 1;
+
+            Marine m2 = new Marine();
+
+            //m2 스팀팩 사용
+            m2.HP = m2.HP - 5;
+            m2.speed = m2.speed + 1;
+
+            //m1 스팀팩 사용
+            m1.HP = m1.HP - 5;
+            m1.speed = m1.speed + 1;
+
+        }// end of method
+    }// end of class
+}
