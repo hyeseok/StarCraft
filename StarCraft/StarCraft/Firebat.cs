91bd1a22e61f8a15e72f20657c7437a71d3a930c
hyeseok <41276412+hyeseok@users.noreply.github.com>
2018-07-17 16:27:21 +0900

    pre_oop

---
+++ StarCraft/StarCraft/Firebat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarCraft
+{
+    class Firebat
+    {
+        public int HP = 60;
+        public int speed = 1;
+    }
+}
--- StarCraft/StarCraft/Program.cs
+++ StarCraft/StarCraft/Program.cs
@@ -12,46 +12,87 @@ namespace StarCraft
             Marine m1 = new Marine();
 
             //m1 스팀팩 사용
-            int hp1 = UseSteamPack(m1, 2); 
+            int hp1 = Marine_UseSteamPack(m1, 2); 
 
             Marine m2 = new Marine();
 
             //m2 스팀팩 사용
-            int hp2 = UseSteamPack(m2, 1);
+            int hp2 = Marine_UseSteamPack(m2, 1);
 
             //m1의 HP를 30만큼 회복
             Heal(m1, 30);
 
-            GetDamaged(m2, 3);
+            Marine_GetDamaged(m2, 3);
+            
+
+            //============================= Firebat Here>
+            Firebat f1 = new Firebat();
+            Firebat_UseSteamPack(f1, 1);
+            Firebat_GetDamaged(f1, 3);
+
+            //============================= HP 출력
+            showHP(m1);
+            
 
             Console.WriteLine("m1의 현재 HP는 "+ m1.HP+ "이고, speed는 "+ m1.speed+ "입니다.");
             Console.WriteLine("m2의 현재 HP는 " + m2.HP + "이고, speed는 "+m2.speed+ "입니다." );
+            Console.WriteLine("f1의 현재 HP는 " + f1.HP + "이고, speed는 "+f1.speed+ "입니다." );
 
         }// end of method
 
-        // 공격을 당할때
-        private static void GetDamaged(Marine m, int enemyAttack)
+        // HP출력 method
+        private static void showHP(Marine m1)
+        {
+            //Console.WriteLine("m1의 현재 HP는 " + m1.HP + "이고, speed는 " + m1.speed + "입니다.");
+            //Console.WriteLine("m1의 현재 HP는 {}이고, speed는 {}입니다.",m1.HP, m1.speed);
+            Console.WriteLine($"m1의 현재 HP는 {m1.HP} 이고, speed는 {m1.speed} 입니다.");
+        }
+
+        // 공격을 당할때 (Marine)
+        private static void Marine_GetDamaged(Marine m, int enemyAttack)
         {
             m.HP -= (enemyAttack * 2);
         }
 
-        // 스팀팩 사용할 때
-        static int UseSteamPack(Marine m, int count)
+        // 공격을 당할때 (Firebat)
+        private static void Firebat_GetDamaged(Firebat f, int enemyAttack)
         {
+            f.HP -= (enemyAttack * 3);
+        }
 
+        // 스팀팩 사용할 때(use Marine)
+        static int Marine_UseSteamPack(Marine m, int count)
+        {
             for (int i = 0; i < count; i++)
             {
-                UseSteamPackCore(m);
+                Marine_UseSteamPackCore(m);
             }
 
             return m.HP;
         }
+        // 스팀팩 사용할 때(use Firebat) 
+        static int Firebat_UseSteamPack(Firebat f, int count)
+        {
 
-        // 스팀팩 사용 코어
-        static void UseSteamPackCore(Marine m)
+            for (int i = 0; i < count; i++)
+            {
+                Firebat_UseSteamPackCore(f);
+            }
+
+            return f.HP;
+        }
+
+        // 스팀팩 사용 코어(Marine)
+        static void Marine_UseSteamPackCore(Marine m)
         {
             m.HP = m.HP - 5;
             m.speed = m.speed + 1;
+        }
+        // 스팀팩 사용 코어(Firebat)
+        static void Firebat_UseSteamPackCore(Firebat f)
+        {
+            f.HP = f.HP - 5;
+            f.speed = f.speed + 1;
         }
 
         // Heal
